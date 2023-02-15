9bb162c4f174fb9780356519a75d840e4e33e7b4
Alexandr Telkov <alexandrtelkov94@gmail.com>
2023-02-15 17:36:31 +0300

    HW2

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,45 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
 public class MealRestController {
-    private MealService service;
+    private final MealService service;
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public Meal get(int id, int userId) {
+        log.info("get meal{}", id);
+        return service.get(id, userId);
+    }
+
+    public Meal save(Meal meal, int userId) {
+        log.info("create meal{}", meal);
+        checkNew(meal);
+        return service.save(meal, userId);
+    }
+
+    public void delete(int id, int userId) {
+        log.info("delete meal{}", id);
+        service.delete(id, userId);
+    }
+
+    public Collection<Meal> getAll(int userId) {
+        log.info("getAll meals");
+        return service.getAll(SecurityUtil.authUserId());
+    }
 
 }
